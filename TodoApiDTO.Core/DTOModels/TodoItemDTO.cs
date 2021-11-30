7e7148e2bee640e618da0cf209e29fce6122eea1
Vlad Berezin <berezinva@outlook.com>
2021-12-01 00:53:21 +0300

    Onion arch - create Core Class lib

---
+++ TodoApiDTO.Core/DTOModels/TodoItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApiDTO.Core.DTOModels
+{
+    #region snippet
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    #endregion
+}
