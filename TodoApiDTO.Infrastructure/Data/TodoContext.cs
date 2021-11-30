e6fcd442a5ab09ab149683c2399d0d1ae82f5139
Vlad Berezin <berezinva@outlook.com>
2021-12-01 00:54:15 +0300

    Add infrastructure class lib, move dbcontext there

---
+++ TodoApiDTO.Infrastructure/Data/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApiDTO.Core.Models;
+
+namespace TodoApiDTO.Infrastructure.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
